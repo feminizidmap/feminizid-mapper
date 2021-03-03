0fc4b789c3ad8877eeaafbd87bb2e9059359316b
lislis <mail@lislis.de>
2021-03-03 17:07:43 +0100

    test(change) add Change model tests

--- spec/models/change_spec.rb
+++ spec/models/change_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Change, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with user and status' do
+    user = FactoryBot.create(:user)
+    change = described_class.create(status: :created, user: user)
+    expect(change).to be_valid
+  end
+
+  it 'is invalid without user' do
+    change = described_class.create(status: :created)
+    change.valid?
+    expect(change.errors[:user]).to include('must exist')
+  end
+
+  it 'is invalid with invalid status' do
+    user = FactoryBot.create(:user)
+    expect { described_class.create(user: user, status: :foo) }.to raise_error(ArgumentError, /valid status/)
+  end
+
+  it 'is invalid without status' do
+    user = FactoryBot.create(:user)
+    expect { described_class.create(user: user) }.to raise_error(ActiveRecord::NotNullViolation, /null value/)
+  end
 end
