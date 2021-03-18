023b705aa70de71bd1a1e789205b62026cc2620c
lislis <mail@lislis.de>
2021-03-18 12:32:53 +0100

    fix(backend) fix 401 when logging out

--- backend/app/controllers/signin_controller.rb
+++ backend/app/controllers/signin_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SigninController < ApplicationController
+  before_action :authorize_access_request!, only: [:destroy]
+
   def create
     user = User.find_by!(email: params[:email])
     if user.authenticate(params[:password])
@@ -21,7 +23,9 @@ class SigninController < ApplicationController
   end
 
   def destroy
-    session = JWTSessions::Session.new(payload: payload)
+    session = JWTSessions::Session.new(payload: payload,
+                                       refresh_by_access_allowed: true,
+                                       namespace: "user_#{payload['user_id']}")
     session.flush_by_access_payload
     render json: :ok
   end
--- backend/spec/controllers/signin_controller_spec.rb
+++ backend/spec/controllers/signin_controller_spec.rb
@@ -3,9 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe SigninController, type: :controller do
+  let(:user) { FactoryBot.create(:user, { password: 'querty' }) }
+
   describe 'POST #create' do
     let(:password) { 'querty' }
-    let(:user) { FactoryBot.create(:user, { password: 'querty' }) }
     let(:user_params) { { email: user.email, password: password } }
 
     it 'returns http success' do
@@ -18,6 +19,35 @@ RSpec.describe SigninController, type: :controller do
     it 'returns unauthorized for invalid params' do
       post :create, params: { email: user.email, password: 'incorrect' }
       expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  describe 'logout DELETE #destroy' do
+    context 'when failing' do
+      it 'returns unauthorized http status' do
+        delete :destroy
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when successful' do
+      it 'returns http success with valid tokens' do
+        payload = { user_id: user.id, aud: [user.role] }
+
+        session = JWTSessions::Session.new(
+          payload: payload,
+          refresh_by_access_allowed: true,
+          namespace: "user_#{user.id}"
+        )
+
+        tokens = session.login
+        request.cookies[JWTSessions.access_cookie] = tokens[:access]
+        request.headers[JWTSessions.csrf_header] = tokens[:csrf]
+
+        delete :destroy
+        expect(response).to have_http_status(:ok)
+        expect(response_json).to eq('ok')
+      end
     end
   end
 end
