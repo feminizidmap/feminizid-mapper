4f15ae2fc635420b0ad64aef8a95909b7e4e6155
lislis <mail@lislis.de>
2021-03-19 23:15:52 +0100

    feat(backend) add system settings table

---
+++ backend/db/migrate/20210319221018_create_system_settings.rb
@@ -0,0 +1,10 @@
+class CreateSystemSettings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :system_settings do |t|
+      t.string :key, null: false
+      t.string :value, null: false
+      t.timestamps
+      t.index :key
+    end
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_145516) do
+ActiveRecord::Schema.define(version: 2021_03_19_221018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2021_03_17_145516) do
     t.string "lang", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "system_settings", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "value", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["key"], name: "index_system_settings_on_key"
   end
 
   create_table "users", force: :cascade do |t|
