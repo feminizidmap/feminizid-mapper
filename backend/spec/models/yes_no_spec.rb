2e8d38cfc20e0805bbee140640512755f4f7bb70
lislis <mail@lislis.de>
2021-04-08 15:44:21 +0200

    add more yesno model spec

--- backend/spec/models/yes_no_spec.rb
+++ backend/spec/models/yes_no_spec.rb
@@ -13,4 +13,63 @@ RSpec.describe YesNo, type: :model do
     yn.valid?
     expect(yn.errors[:answer]).to include("can't be blank")
   end
+
+  describe '.is_yes' do
+    it 'includes everything with yes' do
+      yn = described_class.create!(answer: :yes)
+      expect(described_class.is_yes).to include(yn)
+    end
+
+    it 'excludes everything without yes' do
+      yn = described_class.create!(answer: :no)
+      expect(described_class.is_yes).not_to include(yn)
+    end
+  end
+
+  describe '.is_no' do
+    it 'includes everything with no' do
+      yn = described_class.create!(answer: :no)
+      expect(described_class.is_no).to include(yn)
+    end
+
+    it 'excludes everything without no' do
+      yn = described_class.create!(answer: :unknown)
+      expect(described_class.is_no).not_to include(yn)
+    end
+  end
+
+  describe '.is_unknown' do
+    it 'includes everything with unknown' do
+      yn = described_class.create!(answer: :unknown)
+      expect(described_class.is_unknown).to include(yn)
+    end
+
+    it 'excludes everything without no' do
+      yn = described_class.create!(answer: :yes)
+      expect(described_class.is_unknown).not_to include(yn)
+    end
+  end
+
+  describe '.is_not_applicable' do
+    it 'includes everything with not_applicable' do
+      yn = described_class.create!(answer: :not_applicable)
+      expect(described_class.is_not_applicable).to include(yn)
+    end
+
+    it 'excludes everything without no' do
+      yn = described_class.create!(answer: :no)
+      expect(described_class.is_not_applicable).not_to include(yn)
+    end
+  end
+
+  describe '.is()' do
+    it 'includes everything with parameter' do
+      yn = described_class.create!(answer: :no)
+      expect(described_class.is(:no)).to include(yn)
+    end
+    it 'excludes everything without parameter' do
+      yn = described_class.create!(answer: :unknown)
+      expect(described_class.is(:no)).not_to include(yn)
+    end
+  end
 end
