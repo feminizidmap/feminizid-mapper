6c5fb4293ba34da1816644ff1de847918abc66a3
lislis <mail@lislis.de>
2021-06-08 00:29:41 +0200

    nice delete experience

--- frontend/src/components/cases/ItemComplex.vue
+++ frontend/src/components/cases/ItemComplex.vue
@@ -6,6 +6,14 @@
     <CDate :item="item" />
   </div>
   <div>
+    <template v-if="rmIntervalId">
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped bg-warning progress-bar-animated" role="progressbar" :style="rmTimerLeftWidth" :aria-valuenow="rmTimerLeft" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <p>Permanently deleting</p>
+      <button @click.prevent="stopRm" class="btn btn-outline-primary mt-3">Cancel delete</button>
+    </template>
+    <template v-else>
     <router-link  class="btn btn-outline-primary me-2"
                   :to="{name: 'CaseSingle', params: { caseid: item.id}}">
       <i class="fa fa-eye"></i>
@@ -13,9 +21,12 @@
     <button  class="btn btn-outline-primary me-2">
       <i class="fa fa-edit"></i>
       <span class="visually-hidden">{{ $t('forms.edit') }}</span></button>
-    <button v-if="showIfAdmin()"  class="btn btn-outline-danger">
+    <button v-if="showIfAdmin()"
+            class="btn btn-outline-danger"
+            @click.prevent="rmCase">
       <i class="fa fa-trash-alt"></i>
       <span class="visually-hidden">{{ $t('forms.delete') }}</span></button>
+    </template>
   </div>
 </li>
 </template>
@@ -27,10 +38,51 @@ export default {
   name: 'CaseItemComplex',
   components: { QuickView, CDate },
   props: ['item'],
+  data() {
+    return {
+      rmIntervalId: null,
+      rmTimer: null,
+      rmTimerLeft: null,
+      rmTimerLeftWidth: null,
+      rmMaxTime: 6000
+    }
+  },
   methods: {
     showIfAdmin() {
       return this.$store.getters.isAdmin
     },
+    reallyRmCase() {
+      this.$http.secured.delete(`/case/${this.item.id}`).then(() => {
+        this.$store.commit('removeSingleCase', this.item)
+        this.$store.commit('addAlert', { message: `Deleted item`, type: 'info'})
+      }).catch(function (error) {
+        this.$store.commit('addAlert', { message: `Error bill robinson ${error}`, type: 'error'})
+      })
+    },
+    rmCountdown() {
+      let d = new Date()
+      let diff = d.getTime() -this.rmTimer.getTime()
+
+      this.rmTimerLeft = Math.floor(diff / this.rmMaxTime * 100)
+      this.rmTimerLeftWidth = `width: ${this.rmTimerLeft}%;`
+
+      if (diff > this.rmMaxTime + 1000) {
+        this.reallyRmCase()
+        clearInterval(this.rmIntervalId)
+      }
+    },
+    rmCase() {
+      this.rmTimer = new Date()
+      this.rmTimerLeft = 0
+      this.rmTimerLeftWidth = 'width: 0%;'
+      this.rmIntervalId = setInterval(() => {
+        this.rmCountdown()
+      }, 500)
+    },
+    stopRm() {
+      clearInterval(this.rmIntervalId)
+      this.rmIntervalId = null
+    }
   }
 }
 </script>
